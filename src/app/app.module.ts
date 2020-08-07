ac6ee9a9390f7f500d930b2262da47db450d6c64
Ron Hager-Schaper <ron.hager.mail@gmail.com>
2020-08-07 18:20:10 +0200

    added ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+import { ItemsState } from './store/items.state';
+
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxsModule.forRoot([ItemsState]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    BrowserAnimationsModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
